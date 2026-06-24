4b7e347552b2430a94381be0dccb4a2b9b70f2b5
Abdur Rehman <abdur.rehman8872@gmail.com>
2026-06-24 16:59:46 +0500

    further practice

--- day 4/implementation.py
+++ day 4/implementation.py
@@ -123,4 +123,25 @@ def display_info(name, age):
     print(f"{name } is {age} years old.")
 
 
-display_info("abdur", 22)
+# display_info("abdur", 22)
+
+
+# ==========context managers===========
+
+
+class Open_File:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc, tb):
+        self.file.close()
+
+with Open_File('sample.txt', 'w') as f:
+    f.write('hello how are u')
+
+print(f.closed)
