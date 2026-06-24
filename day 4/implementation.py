3df84ef1a5f40bbb32c49d26180d2b15840dd9e6
Abdur Rehman <abdur.rehman8872@gmail.com>
2026-06-24 16:15:53 +0500

    Implementations files

--- day 4/implementation.py
+++ day 4/implementation.py
@@ -71,14 +71,56 @@ def decorator_function(original_function):
     return wrapper_function
 
 
-@decorator_function
-def display():
-    print("display function ran")
+# @decorator_function
+# def display():
+#     print("display function ran")
 
-@decorator_function
+
+# @decorator_function
+# def display_info(name, age):
+#     print(f"{name } is {age} years old.")
+
+
+# display()
+# display_info("john", 25)
+
+
+from functools import wraps
+
+
+def my_logger(orig_func):
+    import logging
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.basicConfig(
+            filename=f"{orig_func.__name__}.log", level=logging.INFO, force=True
+        )
+
+        logging.info(f"ran with args: {args} and kwargs: {kwargs}")
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print(f"{orig_func.__name__} executed in", t2, "sec")
+        return result
+
+    return wrapper
+
+
+@my_logger
+@my_timer
 def display_info(name, age):
     print(f"{name } is {age} years old.")
 
 
-display()
-display_info("john", 25)
+display_info("abdur", 22)
