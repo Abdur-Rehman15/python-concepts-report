0cc94f68f322f35f8dce8bbc11b92842efe75234
Abdur Rehman <abdur.rehman8872@gmail.com>
2026-06-29 17:14:09 +0500

    matrix mul

--- day 4/matrix_multiplication.py
+++ day 4/matrix_multiplication.py
@@ -21,24 +21,37 @@ def measure_time(orig_func):
 @measure_time
 def multiply_simple_matrices(mat1, mat2):
     res_matrix = []
-    if len(mat1[0]) == len(mat2):
-        for i in range(len(mat1)):
-            temp = []
-            for j in range(len(mat2[0])):
-                sum = 0
-                for k in range(len(mat2)):
-                    sum += mat1[i][k] * mat2[k][j]
-                temp.append(sum)
 
-            res_matrix.append(temp)
+    # 1d vectors both
+    if not isinstance(mat1[0], list) and not isinstance(mat2[0], list):
+        if len(mat1) != len(mat2):
+            raise ValueError("different lengths")
+        for i in range(len(mat1)):
+            res_matrix.append(mat1[i] * mat2[i])
+
+    # 2d vectors both
+    elif isinstance(mat1[0], list) and isinstance(mat2[0], list):
+        if len(mat1[0]) == len(mat2):
+            for i in range(len(mat1)):
+                temp = []
+                for j in range(len(mat2[0])):
+                    sum = 0
+                    for k in range(len(mat2)):
+                        sum += mat1[i][k] * mat2[k][j]
+                    temp.append(sum)
+
+                res_matrix.append(temp)
+
+        else:
+            raise ValueError("non compatible..multiplication not possible")
 
     else:
-        raise ValueError("multiplication not possible")
+        raise ValueError("1d and 2d cant multiply with each other")
 
     return res_matrix
 
 
-print('\n---without numpy---')
+print("\n---without numpy---")
 print(multiply_simple_matrices(matrix1, matrix2))
 
 arr1 = np.array(matrix1)
@@ -49,5 +62,6 @@ arr2 = np.array(matrix2)
 def multiply_numpy_matrices(mat1, mat2):
     return mat1 @ mat2
 
-print('\n---with numpy---')
+
+print("\n---with numpy---")
 print(multiply_numpy_matrices(arr1, arr2))
